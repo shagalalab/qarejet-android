da7c7a16b6041f56c5b1e5fd45a64ea8e113367a
MaratKhakim <marakhakim@gmail.com>
2020-10-26 23:56:38 +0100

    Add RecordsFragment #10

--- app/src/main/java/com/shagalalab/qarejet/presentation/MainFragment.kt
+++ app/src/main/java/com/shagalalab/qarejet/presentation/MainFragment.kt
@@ -1,6 +1,32 @@
 package com.shagalalab.qarejet.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import com.shagalalab.qarejet.R
+import com.shagalalab.qarejet.databinding.FragmentMainBinding
 
-class MainFragment : Fragment(R.layout.fragment_main)
+class MainFragment : Fragment() {
+    private lateinit var fragmentMainBinding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentMainBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        return fragmentMainBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = Navigation.findNavController(requireActivity(), R.id.mainNavHost)
+        fragmentMainBinding.bottomNavigation.setupWithNavController(navController)
+    }
+}

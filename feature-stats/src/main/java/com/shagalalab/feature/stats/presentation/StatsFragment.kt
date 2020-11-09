be042571dd15ff30864ed348aee8d44d8454e6aa
MambetniyazovAmir <mr.amirniyaz@mail.ru>
2020-11-09 15:53:47 +0500

    added two tabs

--- feature-stats/src/main/java/com/shagalalab/feature/stats/presentation/StatsFragment.kt
+++ feature-stats/src/main/java/com/shagalalab/feature/stats/presentation/StatsFragment.kt
@@ -1,6 +1,27 @@
 package com.shagalalab.feature.stats.presentation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 import com.shagalalab.feature.stats.R
+import com.shagalalab.feature.stats.util.Constants
+import kotlinx.android.synthetic.main.fragment_stats.*
 
-class StatsFragment : Fragment(R.layout.fragment_stats)
+class StatsFragment : Fragment(R.layout.fragment_stats) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        statsTabLayout.getTabAt(Constants.TRANSACTION_TYPE_EXPENSE)?.select()
+        statsTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+            }
+        })
+    }
+}
---
+++ feature-stats/src/main/java/com/shagalalab/feature/stats/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.shagalalab.feature.stats.util
+
+object Constants {
+    const val TRANSACTION_TYPE_INCOME = 0
+    const val TRANSACTION_TYPE_EXPENSE = 1
+}

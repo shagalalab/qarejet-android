a7e6733cf107fad1a7da51658b779e1927035cd1
MambetniyazovAmir <mr.amirniyaz@mail.ru>
2020-11-14 15:59:24 +0500

    dataBingding implemented

--- feature-stats/src/main/java/com/shagalalab/feature/stats/presentation/StatsFragment.kt
+++ feature-stats/src/main/java/com/shagalalab/feature/stats/presentation/StatsFragment.kt
@@ -1,29 +1,48 @@
 package com.shagalalab.feature.stats.presentation
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayout
 import com.shagalalab.feature.stats.R
-import com.shagalalab.feature.stats.util.Constants
-import kotlinx.android.synthetic.main.fragment_stats.*
+import com.shagalalab.feature.stats.databinding.FragmentStatsBinding
 
-class StatsFragment : Fragment(R.layout.fragment_stats) {
+class StatsFragment : Fragment() {
+
+    companion object {
+        const val TRANSACTION_TYPE_INCOME = 0
+        const val TRANSACTION_TYPE_EXPENSE = 1
+    }
+
+    private lateinit var fragmentStatsBinding: FragmentStatsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentStatsBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_stats, container, false)
+        return fragmentStatsBinding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        statsTabLayout.getTabAt(Constants.TRANSACTION_TYPE_EXPENSE)?.select()
+        fragmentStatsBinding.statsTabLayout.getTabAt(TRANSACTION_TYPE_EXPENSE)?.select()
+        fragmentStatsBinding.statsTabLayout.addOnTabSelectedListener(object :
+                TabLayout.OnTabSelectedListener {
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+                }
 
-        statsTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-            }
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+                }
 
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-            }
-
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                tvMessage.text = "${tab?.text} tab selected"
-            }
-        })
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    fragmentStatsBinding.tvMessage.text = "${tab?.text} tab selected"
+                }
+            })
     }
 }
--- feature-stats/src/main/java/com/shagalalab/feature/stats/util/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.shagalalab.feature.stats.util
-
-object Constants {
-    const val TRANSACTION_TYPE_INCOME = 0
-    const val TRANSACTION_TYPE_EXPENSE = 1
-}

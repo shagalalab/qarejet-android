d34c9b671555825658edc518431e7c2df5ae25d5
MambetniyazovAmir <mr.amirniyaz@mail.ru>
2020-11-16 12:20:23 +0500

    code cleaning

--- feature-stats/src/main/java/com/shagalalab/feature/stats/presentation/StatsFragment.kt
+++ feature-stats/src/main/java/com/shagalalab/feature/stats/presentation/StatsFragment.kt
@@ -1,38 +1,25 @@
 package com.shagalalab.feature.stats.presentation
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayout
 import com.shagalalab.feature.stats.R
 import com.shagalalab.feature.stats.databinding.FragmentStatsBinding
 
-class StatsFragment : Fragment() {
+class StatsFragment : Fragment(R.layout.fragment_stats) {
 
     companion object {
-        const val TRANSACTION_TYPE_INCOME = 0
         const val TRANSACTION_TYPE_EXPENSE = 1
     }
 
-    private lateinit var fragmentStatsBinding: FragmentStatsBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        fragmentStatsBinding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_stats, container, false)
-        return fragmentStatsBinding.root
-    }
+    private lateinit var binding: FragmentStatsBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fragmentStatsBinding.statsTabLayout.getTabAt(TRANSACTION_TYPE_EXPENSE)?.select()
-        fragmentStatsBinding.statsTabLayout.addOnTabSelectedListener(object :
+        binding = FragmentStatsBinding.bind(view)
+        binding.statsTabLayout.getTabAt(TRANSACTION_TYPE_EXPENSE)?.select()
+        binding.statsTabLayout.addOnTabSelectedListener(object :
                 TabLayout.OnTabSelectedListener {
                 override fun onTabReselected(tab: TabLayout.Tab?) {
                 }
@@ -41,7 +28,7 @@ class StatsFragment : Fragment() {
                 }
 
                 override fun onTabSelected(tab: TabLayout.Tab?) {
-                    fragmentStatsBinding.tvMessage.text = "${tab?.text} tab selected"
+                    binding.tvMessage.text = "${tab?.text} tab selected"
                 }
             })
     }

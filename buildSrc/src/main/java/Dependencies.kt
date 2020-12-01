84c343db0973734db5ae46cbd0509837c5760d90
khareken <ernazartorebaev@gmail.com>
2020-12-01 23:24:06 +0500

    removed all unnecessary lines and spaces

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -34,7 +34,6 @@ object Modules {
     const val feature_splash = ":feature-splash"
     const val feature_stats = ":feature-stats"
     const val feature_transaction = ":feature-transaction"
-
 }
 
 object Libs {
